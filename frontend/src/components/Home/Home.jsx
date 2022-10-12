25979b4b618f7e20f1107b49c1be7b8fea8a0f34
Heytemisan <temyicy@gmail.com>
2022-10-12 21:11:55 +0100

    feat: code cleanup

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,17 +1,41 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
 
 const Home = () => {
+  const spring = {
+    type: "spring",
+    damping: 10,
+    stiffness: 100,
+  };
+
+  const options = {
+    initial: {
+      x: "-100%",
+      opacity: 0,
+    },
+    whileInView: {
+      x: 0,
+      opacity: 1,
+    },
+    transition:spring
+  }
   return (
     <section className="home">
       <div>
-        <h1>BURGER KINGS</h1>
-        <p>Give yourself a tasty burger.</p>
+        <motion.h1
+          {...options}
+        >
+          BURGER KING
+        </motion.h1>
+        <motion.p
+          {...options}
+        >
+          Give yourself a tasty burger.
+        </motion.p>
       </div>
-      <a href="#menu">
-        Explore menu
-      </a>
+      <a href="#menu">Explore menu</a>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
