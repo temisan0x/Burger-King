4f581444d09f0fd510d0b1338aa5491eb1b2e5ef
Heytemisan <temyicy@gmail.com>
2022-10-22 18:42:09 +0100

    feat: code cleanup

--- server/app.js
+++ server/app.js
@@ -1,17 +1,31 @@
 import express from "express";
 import dotenv from "dotenv";
-import {connectPassport} from "./utils/Provider.js"
+import { connectPassport } from "./utils/Provider.js";
+import session from "express-session";
+import passport from "passport";
 //import Routes
 import userRoute from "./routes/user.js";
 
 const app = express();
 
 dotenv.config({
-    path: "./config/config.env"
+  path: "./config/config.env",
 });
+//middleware
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.authenticate("session"));
+app.use(passport.initialize());
+app.use(passport.session());
+connectPassport();
 
 //userRoute
 app.use("/api/v1", userRoute);
-connectPassport();
 
 export default app;
--- server/routes/user.js
+++ server/routes/user.js
@@ -8,6 +8,14 @@ router.get(
     passport.authenticate("google",{
         scope:["profile"]
     })
+);
+
+router.get(
+    "/login",
+    passport.authenticate("google",{
+        scope:["profile"],
+        successRedirect: process.env.FRONTEND_URL
+    })
 )
 
 export default router; 
